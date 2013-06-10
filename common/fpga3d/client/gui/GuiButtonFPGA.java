e33546aae97eadc3c761f62cba9037d7a0708183
rhilenova <rhilenova@gmail.com>
2013-06-09 23:21:21 -0400

    Added more buttons for setting LUT values. Not saved.

--- common/fpga3d/client/gui/GuiButtonFPGA.java
+++ common/fpga3d/client/gui/GuiButtonFPGA.java
@@ -3,9 +3,9 @@ package fpga3d.client.gui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 
-public class GuiButtonFPGAInput extends GuiButton
+public class GuiButtonFPGA extends GuiButton
 {
-	public GuiButtonFPGAInput(int par1, int par2, int par3, int par4, int par5,
+	public GuiButtonFPGA(int par1, int par2, int par3, int par4, int par5,
 			String par6Str) {
 		super(par1, par2, par3, par4, par5, par6Str);
 	}
--- common/fpga3d/client/gui/GuiScreenFPGA.java
+++ common/fpga3d/client/gui/GuiScreenFPGA.java
@@ -7,6 +7,9 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 
 public class GuiScreenFPGA extends GuiContainer
 {
+    int[] number_vals = {0, 0, 0, 0, 0, 0, 0, 0};
+    int num_endpoints;
+    int num_numbers;
     
 	public GuiScreenFPGA()
 	{
@@ -24,17 +27,42 @@ public class GuiScreenFPGA extends GuiContainer
         super.initGui();
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
-        int[][] button_locs =
+        int[][] endpoint_locs =
+        	// Inputs
         	{{13, 114}, {13, 122}, {13, 130}, {13, 138}, {13, 146}, {13, 154},
+        	// LUT Inputs
         	{36, 45}, {36, 53}, {36, 61},
+        	// LUT Outputs
         	{119, 49}, {119, 57},
+        	// FF Inputs
         	{139, 118}, {139, 126}, {139, 151}, {139, 159},
+        	// FF Outputs
         	{180, 118}, {180, 126}, {180, 151}, {180, 159},
+        	// Outputs
         	{204, 26}, {204, 34}, {204, 42}, {204, 50}, {204, 58}, {204, 66}};
-        for (int x = 0; x < button_locs.length; ++x)
+        num_endpoints = endpoint_locs.length;
+        for (int x = 0; x < endpoint_locs.length; ++x)
         {
-        	GuiButton temp =new GuiButtonFPGAInput(x, k + button_locs[x][0],
-        										   l + button_locs[x][1], 6, 6, null);
+        	GuiButton temp = new GuiButtonFPGA(x, k + endpoint_locs[x][0],
+        								       l + endpoint_locs[x][1], 6, 6, null);
+        	temp.drawButton = false;
+        	buttonList.add(temp);
+        }
+        int[][] number_locs =
+        	// LUT
+        	{{100, 24}, {106, 24},
+        	 {100, 34}, {106, 34},
+           	 {100, 44}, {106, 44},
+           	 {100, 54}, {106, 54},
+           	 {100, 64}, {106, 64},
+           	 {100, 74}, {106, 74},
+           	 {100, 84}, {106, 84},
+           	 {100, 94}, {106, 94}};
+        num_numbers = number_locs.length;
+        for (int x = 0; x < number_locs.length; ++x)
+        {
+        	GuiButton temp = new GuiButtonFPGA(x + endpoint_locs.length, k + number_locs[x][0],
+				       						   l + number_locs[x][1], 5, 7, null);
         	temp.drawButton = false;
         	buttonList.add(temp);
         }
@@ -46,6 +74,12 @@ public class GuiScreenFPGA extends GuiContainer
     protected void actionPerformed(GuiButton par1GuiButton)
     {
     	System.out.println("Button " + par1GuiButton.id);
+    	if (par1GuiButton.id >= num_endpoints && par1GuiButton.id < num_endpoints + num_numbers)
+    	{
+        	int number_id = (par1GuiButton.id - num_endpoints) / 2;
+        	int number_mod = (par1GuiButton.id - num_endpoints) % 2;
+        	number_vals[number_id] = number_vals[number_id] ^ (0x10 >> (4 * number_mod));
+    	}
     }
 
 	@Override
@@ -61,14 +95,10 @@ public class GuiScreenFPGA extends GuiContainer
     	fontRenderer.drawString("110", 50, 84, 4210752);
     	fontRenderer.drawString("111", 50, 94, 4210752);
     	
-    	fontRenderer.drawString("00", 100, 24, 4210752);
-    	fontRenderer.drawString("00", 100, 34, 4210752);
-    	fontRenderer.drawString("00", 100, 44, 4210752);
-    	fontRenderer.drawString("00", 100, 54, 4210752);
-    	fontRenderer.drawString("00", 100, 64, 4210752);
-    	fontRenderer.drawString("00", 100, 74, 4210752);
-    	fontRenderer.drawString("00", 100, 84, 4210752);
-    	fontRenderer.drawString("00", 100, 94, 4210752);
+    	for (int idx = 0; idx < 8; ++idx)
+    	{
+        	fontRenderer.drawString(String.format("%02X", number_vals[idx]), 100, 24 + (10 * idx), 4210752);
+    	}
     	
     	fontRenderer.drawString("D", 149, 117, 4210752);
     	fontRenderer.drawString("Clk", 149, 127, 4210752);
